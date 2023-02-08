1ac4e86ad52fd1206e3a5041c9a2e81168f33075
JhaanHoltzhausen <115926679+JhaanHoltzhausen@users.noreply.github.com>
2023-02-08 13:56:16 +0200

    merge sort and tests

--- tests/test_my_sort.py
+++ tests/test_my_sort.py
@@ -1,8 +1,23 @@
-from ..my_sort import *
+from ..sort import *
 
-float_list = [10.5, 11.6, 3.4, 5.7, 11]
+# import random
+
+float_list = make_list(100, float)
+int_list = make_list(100, int)
+
+lis = [3.4, 5.7, 10.5, 11, 11.6]
 
 
-def test_bubble():
-    assert bubble_sort(list(range(10,0,-1))) == list(range(1, 11))
-    assert bubble_sort(float_list) == [3.4, 5.7, 10.5, 11, 11.6]
+def test_continuate():
+    assert bubble_sort(int_list) == merge_sort(int_list)
+    assert bubble_sort(float_list) == merge_sort(float_list)
+
+
+def test_bubble_sort():
+    assert bubble_sort(list(range(10, 0, -1))) == list(range(1, 11))
+    assert bubble_sort(lis) == [3.4, 5.7, 10.5, 11, 11.6]
+
+
+def test_merge_sort():
+    assert merge_sort(list(range(10, 0, -1))) == list(range(1, 11))
+    assert merge_sort(lis) == [3.4, 5.7, 10.5, 11, 11.6]
